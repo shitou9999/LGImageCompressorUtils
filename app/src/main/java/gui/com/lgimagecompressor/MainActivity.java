07616c795e24537c22ba5194dcb5cabd2f93b8f0
zhangwei <zhangwei@zhanyaa.com>
2016-07-04 09:14:37 +0800

    图片压缩处理

---
+++ app/src/main/java/gui/com/lgimagecompressor/MainActivity.java
@@ -0,0 +1,30 @@
+package gui.com.lgimagecompressor;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.basic_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,BasicCompressActivity.class);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.from_service).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ServiceCompressActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
